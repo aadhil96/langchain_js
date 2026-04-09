5906ab4c0830c1a84645a99d4b8e151f9565f9bc
aadhil96 <aadhil.imam@gmail.com>
2026-04-09 21:17:01 +0530

    runnable

--- src/runnable.js
+++ src/runnable.js
@@ -4,3 +4,5 @@ import { initChatModel } from "langchain";
 import { AIMessage, HumanMessage, SystemMessage } from "langchain";
 
 const model = await initChatModel("gpt-4o-mini");
+
+const response = await model.invoke("Hello ?")
